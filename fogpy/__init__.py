1c798a3e69a0ab8e3e5e8516770dee600158dc7a
Gerrit Holl <gerrit.holl@dwd.de>
2019-05-29 15:14:48 +0200

    Disable plotting functions that use mpop
    Disable plotting functions that use mpop and simple images, I need to
    implement XRImage, xarray, dask completely before I can use those again.

--- fogpy/__init__.py
+++ fogpy/__init__.py
@@ -3,21 +3,22 @@
 # Copyright (c) 2017
 # Author(s):
 #   Thomas Leppelt <thomas.leppelt@dwd.de>
+#   Gerrit Holl    <gerrit.holl@dwd.de>
 
 # This file is part of the fogpy package.
 
-# mpop is free software: you can redistribute it and/or modify it
+# fogpy is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# mpop is distributed in the hope that it will be useful, but
+# fogpy is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with mpop.  If not, see <http://www.gnu.org/licenses/>.
+# along with fogpy.  If not, see <http://www.gnu.org/licenses/>.
 
 """PP Package initializer.
 """
