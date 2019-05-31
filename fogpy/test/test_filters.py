898490bdfbdf3633bcd0c086cd8fe565acb986f6
Gerrit Holl <gerrit.holl@dwd.de>
2019-05-31 14:36:18 +0200

    use np.testing.assert_almost_equal
    use np.testing.assert_almost_equal instead of self.assertAlmostEqual, as
    the former can handle numpy arrays or not

--- fogpy/test/test_filters.py
+++ fogpy/test/test_filters.py
@@ -141,11 +141,11 @@ class Test_CloudFilter(unittest.TestCase):
         testfilter = CloudFilter(self.input['ir108'], **self.input)
         ret, mask = testfilter.apply()
         # Evaluate results
-        self.assertAlmostEqual(self.ir108[0, 0], 244.044000086)
-        self.assertAlmostEqual(self.ir039[20, 100], 269.573815979)
-        self.assertAlmostEqual(testfilter.minpeak, -19.744686196671914)
-        self.assertAlmostEqual(testfilter.maxpeak, 1.11645277953)
-        self.assertAlmostEqual(testfilter.thres, -3.51935588185)
+        numpy.testing.assert_almost_equal(self.ir108[0, 0], 244.044000086)
+        numpy.testing.assert_almost_equal(self.ir039[20, 100], 269.573815979)
+        numpy.testing.assert_almost_equal(testfilter.minpeak, -19.744686196671914)
+        numpy.testing.assert_almost_equal(testfilter.maxpeak, 1.11645277953)
+        numpy.testing.assert_almost_equal(testfilter.thres, -3.51935588185)
         self.assertEqual(np.sum(testfilter.mask), 20551)
 
     def test_cloud_filter_plot(self):
@@ -156,11 +156,11 @@ class Test_CloudFilter(unittest.TestCase):
         testfilter.plot_filter(save=True)
         testfilter.plot_filter(save=True, area=area_def, type='tif')
         # Evaluate results
-        self.assertAlmostEqual(self.ir108[0, 0], 244.044000086)
-        self.assertAlmostEqual(self.ir039[20, 100], 269.573815979)
-        self.assertAlmostEqual(testfilter.minpeak, -19.744686196671914)
-        self.assertAlmostEqual(testfilter.maxpeak, 1.11645277953)
-        self.assertAlmostEqual(testfilter.thres, -3.51935588185)
+        numpy.testing.assert_almost_equal(self.ir108[0, 0], 244.044000086)
+        numpy.testing.assert_almost_equal(self.ir039[20, 100], 269.573815979)
+        numpy.testing.assert_almost_equal(testfilter.minpeak, -19.744686196671914)
+        numpy.testing.assert_almost_equal(testfilter.maxpeak, 1.11645277953)
+        numpy.testing.assert_almost_equal(testfilter.thres, -3.51935588185)
         self.assertEqual(np.sum(testfilter.mask), 20551)
 
     def test_masked_cloud_filter(self):
@@ -170,11 +170,11 @@ class Test_CloudFilter(unittest.TestCase):
         ret, mask = testfilter.apply()
 
         # Evaluate results
-        self.assertAlmostEqual(self.ir108[0, 0], 244.044000086)
-        self.assertAlmostEqual(self.ir039[20, 100], 269.573815979)
-        self.assertAlmostEqual(testfilter.minpeak, -19.744686196671914)
-        self.assertAlmostEqual(testfilter.maxpeak, 1.11645277953)
-        self.assertAlmostEqual(testfilter.thres, -3.51935588185)
+        numpy.testing.assert_almost_equal(self.ir108[0, 0], 244.044000086)
+        numpy.testing.assert_almost_equal(self.ir039[20, 100], 269.573815979)
+        numpy.testing.assert_almost_equal(testfilter.minpeak, -19.744686196671914)
+        numpy.testing.assert_almost_equal(testfilter.maxpeak, 1.11645277953)
+        numpy.testing.assert_almost_equal(testfilter.thres, -3.51935588185)
         self.assertEqual(np.sum(testfilter.mask), 20551)
         self.assertEqual(np.sum(testfilter.inmask), 4653)
         self.assertEqual(testfilter.new_masked, 15922)
@@ -186,11 +186,11 @@ class Test_CloudFilter(unittest.TestCase):
 
         # Evaluate results
         self.assertEqual(testfilter.ccl.squeeze().shape, (141, 298))
-        self.assertAlmostEqual(round(testfilter.ccl[95, 276], 3), 0.544)
-        self.assertAlmostEqual(round(testfilter.ccl[29, 216], 3), 1)
-        self.assertAlmostEqual(round(testfilter.ccl[78, 45], 3), 0.303)
-        self.assertAlmostEqual(round(testfilter.ccl[61, 261], 3), 0)
-        self.assertEqual(round(np.nanmax(testfilter.cm_diff), 2), 3.43)
+        numpy.testing.assert_almost_equal(testfilter.ccl[95, 276], 0.544)
+        numpy.testing.assert_almost_equal(testfilter.ccl[29, 216], 1)
+        numpy.testing.assert_almost_equal(testfilter.ccl[78, 45], 0.303)
+        numpy.testing.assert_almost_equal(testfilter.ccl[61, 261], 0)
+        numpy.testing.assert_almost_equal(np.nanmax(testfilter.cm_diff), 3.43)
         self.assertTrue(all(testfilter.ccl[testfilter.cm_diff <
                                            testfilter.thres] > 0.5))
         self.assertTrue(all(testfilter.ccl[testfilter.cm_diff >
@@ -230,10 +230,10 @@ class Test_SnowFilter(unittest.TestCase):
         ret, mask = testfilter.apply()
 
         # Evaluate results
-        self.assertAlmostEqual(self.ir108[0, 0], 244.044000086)
-        self.assertAlmostEqual(self.vis008[25, 100], 13.40515625)
-        self.assertAlmostEqual(testfilter.ndsi[30, 214], 0.12547279)
-        self.assertAlmostEqual(testfilter.ndsi[135, 170], 0.62573861)
+        numpy.testing.assert_almost_equal(self.ir108[0, 0], 244.044000086)
+        numpy.testing.assert_almost_equal(self.vis008[25, 100], 13.40515625)
+        numpy.testing.assert_almost_equal(testfilter.ndsi[30, 214], 0.12547279)
+        numpy.testing.assert_almost_equal(testfilter.ndsi[135, 170], 0.62573861)
         self.assertEqual(np.sum(testfilter.mask), 577)
 
 
@@ -270,11 +270,11 @@ class Test_IceCloudFilter(unittest.TestCase):
         ret, mask = testfilter.apply()
 
         # Evaluate results
-        self.assertAlmostEqual(self.ir108[0, 0], 244.044000086)
-        self.assertAlmostEqual(self.vis008[25, 100], 13.40515625)
-        self.assertAlmostEqual(testfilter.ic_diff[50, 50], -0.91323156)
-        self.assertAlmostEqual(testfilter.ic_diff[110, 70], 3.05561071)
-        self.assertAlmostEqual(testfilter.ic_diff[126, 144], 3.05652842)
+        numpy.testing.assert_almost_equal(self.ir108[0, 0], 244.044000086)
+        numpy.testing.assert_almost_equal(self.vis008[25, 100], 13.40515625)
+        numpy.testing.assert_almost_equal(testfilter.ic_diff[50, 50], -0.91323156)
+        numpy.testing.assert_almost_equal(testfilter.ic_diff[110, 70], 3.05561071)
+        numpy.testing.assert_almost_equal(testfilter.ic_diff[126, 144], 3.05652842)
         self.assertEqual(np.sum(testfilter.mask), 36632)
 
 
@@ -316,12 +316,12 @@ class Test_CirrusCloudFilter(unittest.TestCase):
         ret, mask = testfilter.apply()
 
         # Evaluate results
-        self.assertAlmostEqual(self.ir108[0, 0], 244.044000086)
-        self.assertAlmostEqual(self.vis008[25, 100], 13.40515625)
+        numpy.testing.assert_almost_equal(self.ir108[0, 0], 244.044000086)
+        numpy.testing.assert_almost_equal(self.vis008[25, 100], 13.40515625)
         self.assertEqual(np.sum(testfilter.bt_ci_mask |
                                 testfilter.strong_ci_mask),
                          np.sum(testfilter.mask))
-        self.assertAlmostEqual(testfilter.bt_thres[50, 50], 1.1)
+        numpy.testing.assert_almost_equal(testfilter.bt_thres[50, 50], 1.1)
         self.assertGreater(testfilter.bt_diff[50, 50], testfilter.bt_thres[50,
                                                                            50])
         self.assertLess(testfilter.strong_ci_diff[110, 70], 0)
@@ -375,9 +375,9 @@ class Test_WaterCloudFilter(unittest.TestCase):
                                            testfilter.ir039)
         testmean = np.nanmean(cloud_free_ma[140, :])
         self.assertEqual(testfilter.lat_cloudfree[140], testmean)
-        self.assertAlmostEqual(np.sum(~testfilter.cloudmask) +
+        numpy.testing.assert_almost_equal(np.sum(~testfilter.cloudmask) +
                                np.sum(testfilter.cloudmask), 42018)
-        self.assertAlmostEqual(np.sum(testfilter.cloudmask), 20551)
+        numpy.testing.assert_almost_equal(np.sum(testfilter.cloudmask), 20551)
         self.assertEqual(np.sum(testfilter.mask), 19857)
         self.assertEqual(testfilter.line, 141)
 
