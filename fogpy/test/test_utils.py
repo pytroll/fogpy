09ce098d39ac6eaec62b88de9786d0a8f228a32b
Gerrit Holl <gerrit.holl@dwd.de>
2020-06-09 16:29:32 +0200

    Correct logmessage when downloading DEM
    Correct the log message when downloading the DEM.

--- fogpy/test/test_utils.py
+++ fogpy/test/test_utils.py
@@ -1,13 +1,17 @@
+import logging
+
 import pytest
 import unittest.mock
 
 
 @unittest.mock.patch("requests.get")
-def test_dl_dem(rg, tmp_path):
+def test_dl_dem(rg, tmp_path, caplog):
     from fogpy.utils import dl_dem
     rg.return_value.content = b"12345"
 
-    dl_dem(tmp_path / "foo")
+    with caplog.at_level(logging.INFO):
+        dl_dem(tmp_path / "foo")
+    assert f"Downloading https://zenodo.org/record/3885398/files/foo to {tmp_path / 'foo'!s}" in caplog.text
     assert (tmp_path / "foo").exists()
     assert (tmp_path / "foo").open(mode="rb").read() == b"12345"
     with pytest.raises(FileExistsError):
